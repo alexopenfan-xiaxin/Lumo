4667fd8e0beaa7f753eee0b288de578f93647772
alexopenfan-xiaxin <alexopenfan-xiaxin@users.noreply.github.com>
2026-07-17 16:38:55 +0800

    test: reveal lazy chat reply before assertion

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -22,6 +22,8 @@ void main() {
 
     await tester.pump(const Duration(seconds: 1));
     await tester.pumpAndSettle();
+    await tester.drag(find.byType(ListView), const Offset(0, -300));
+    await tester.pumpAndSettle();
     expect(find.textContaining('最明显的感受'), findsOneWidget);
   });
 }
